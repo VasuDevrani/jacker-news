21bc72704a6877487b390f10a09323e4f6432619
VasuDevrani <vasu2002devrani@gmail.com>
2024-10-15 01:18:15 +0530

    setup lucia auth and auth routes

--- server/adapter.ts
+++ server/adapter.ts
@@ -1,7 +1,9 @@
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 import postgres from "postgres";
 import { z } from "zod";
+import { sessionTable, userTable } from "./db/schemas/auth";
 
 const EnvSchema = z.object({
   DATABASE_URL: z.string().url(),
@@ -10,7 +12,18 @@ const EnvSchema = z.object({
 const processEnv = EnvSchema.parse(process.env);
 const queryClient = postgres(processEnv.DATABASE_URL);
 
-const db = drizzle(queryClient);
-const result = await db.execute("SELECT 1");
+export const db = drizzle(queryClient, {
+    schema: {
+        user: userTable,
+        session: sessionTable,
+    },
+});
+// const result = await db.execute("SELECT 1");
 
-console.log(result);
+// console.log(result);
+
+export const adapter = new DrizzlePostgreSQLAdapter(
+  db,
+  sessionTable,
+  userTable,
+);
--- shared/types.ts
+++ shared/types.ts
@@ -1,18 +1,32 @@
+import { z } from "zod";
+
 export type SuccessResponse<T = void> = {
   success: true;
   message: string;
 } & (T extends void ? {} : { data: T });
-
-const data: SuccessResponse<{ id: number }> = {
-  success: true,
-  message: "Post Created",
-  data: {
-    id: 1,
-  },
-};
 
 export type ErrorResponse = {
   success: false;
   error: string;
   isFormError?: boolean;
 };
+
+export const loginSchema = z.object({
+  username: z
+    .string()
+    .min(3)
+    .max(31)
+    .regex(/^[a-zA-Z0-9_]+$/),
+  password: z.string().min(3).max(255),
+});
+
+// export const createPostSchema = insertPostSchema
+//   .pick({
+//     title: true,
+//     url: true,
+//     content: true,
+//   })
+//   .refine((data) => data.url || data.content, {
+//     message: "Either URL or Content must be provided",
+//     path: ["url", "content"],
+//   });
